4d495ac60947162447de8fe1ae21ddfdeff2c2ee
p6352138 <coolpwh@qq.com>
2015-02-05 19:57:22 +0800

    1.0.0.2.5.3
    NGrid.cs架构设计

--- scripts/GameLogic/Navigation/PathGrid/NGrid.cs
+++ scripts/GameLogic/Navigation/PathGrid/NGrid.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 namespace GameLogic.Navigation{
 	public class NGrid  {
+		#region Contants
+		protected static readonly Vector3 XAxis;
+		protected static readonly Vector3 YAxis;
+		private static readonly float Depth;
+		#endregion
 
+		#region Fields
+		protected int m_Rows;
+		protected int m_Columns;
+		#endregion
 	}
 }
