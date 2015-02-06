9cac36d56e7843a4bb50447710438d960d782596
p6352138 <coolpwh@qq.com>
2015-02-06 12:35:58 +0800

    1.0.0.2.6.1
    实体模块初步设计

---
+++ scripts/GameEntity/CCearcue/CCearcueMgr.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using AppUtility;
+
+namespace GameEntity{
+	class CCearcueMgr : Singleton<CCearcueMgr> {
+
+		#region Fields
+		private CTerrian m_curTerrian;
+		private CPlayer m_curPlayer;
+		#endregion
+
+		#region public function
+		/*
+		protected CCearcueMgr(){
+			m_curTerrian = new CTerrian();
+			m_curPlayer = new CPlayer();
+		}*/
+
+		public static void CreateCearcue(CCearcueType type)
+		{
+			if(type == CCearcueType.Player){
+			}
+			else if(type == CCearcueType.Terrian){
+			}
+		}
+		#endregion
+	}
+}
--- scripts/GameLogic/Navigation/PathGrid/NGrid.cs
+++ scripts/GameLogic/Navigation/PathGrid/NGrid.cs
@@ -5,13 +5,50 @@ namespace GameLogic.Navigation{
 	public class NGrid  {
 		#region Contants
 		protected static readonly Vector3 XAxis;
-		protected static readonly Vector3 YAxis;
+		protected static readonly Vector3 ZAxis;
 		private static readonly float Depth;
 		#endregion
 
 		#region Fields
-		protected int m_Rows;
-		protected int m_Columns;
+		protected static int m_Rows;
+		protected static int m_Columns;
+		protected static float m_cellSize;
+		private static Vector3 m_origin;
 		#endregion
+
+		#region Properties
+		public static float Width
+		{
+			get{return m_Rows*m_cellSize;}
+		}
+
+		public static float Height
+		{
+			get{return m_Columns*m_cellSize;}
+		}
+		#endregion
+
+		static NGrid(){
+			XAxis = new Vector3(1,0,0);
+			ZAxis = new Vector3(0,0,1);
+			Depth = 1.0f;
+		}
+
+		public static void DebugDraw()
+		{
+			Vector3 startPos,endPos;
+
+			for(int i=0;i<m_Rows+1;i++){
+				startPos = m_origin + i * XAxis * m_cellSize;
+				endPos = startPos + i * ZAxis * Width;
+				Debug.DrawLine(startPos,endPos);
+			}
+
+			for(int i=0;i<m_Columns+1;i++){
+				startPos = m_origin + i * ZAxis * m_cellSize;
+				endPos = startPos + i * XAxis * Height;
+				Debug.DrawLine(startPos,endPos);
+			}
+		}
 	}
 }
