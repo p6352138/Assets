3f0b65b02469fe8fe64065c12929db757b390e01
p6352138 <coolpwh@qq.com>
2015-03-27 18:21:27 +0800

    1.0.0.3.27.2

--- scripts/NetWork/SocketTest/SocketTest.cs
+++ scripts/NetWork/SocketTest/SocketTest.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System;
 
-public class SocketTest {
+public class SocketTest : MonoBehaviour {
+
+    public string IP;
+    public int port;
+
+    Socket socket;
 
 	// Use this for initialization
 	void Start () {
-	
+        IPEndPoint point = null;
+        IPAddress address = IPAddress.Parse(IP);
+        point = new IPEndPoint(address, port);
+
+        try
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        catch(Exception e)
+        {
+            throw (e);
+        }
+        
+
+
 	}
 	
 	// Update is called once per frame
